feceb84e242e6ffd492069afa023e8efb5d463fa
Schplitt <96245357+schplitt@users.noreply.github.com>
2025-12-05 23:17:13 +0100

    feat: add ignores preset (#3)

---
+++ src/globs.ts
@@ -0,0 +1,39 @@
+
+
+
+export const IGNORE_GLOBS = [
+  '**/node_modules',
+  '**/dist',
+  '**/package-lock.json',
+  '**/yarn.lock',
+  '**/pnpm-lock.yaml',
+  '**/bun.lockb',
+
+  '**/output',
+  '**/coverage',
+  '**/temp',
+  '**/.temp',
+  '**/tmp',
+  '**/.tmp',
+  '**/.history',
+  '**/.vitepress/cache',
+  '**/.nuxt',
+  "**/.nitro",
+  '**/.next',
+  '**/.svelte-kit',
+  '**/.vercel',
+  '**/.changeset',
+  '**/.idea',
+  '**/.cache',
+  '**/.output',
+  '**/.vite-inspect',
+  '**/.yarn',
+  '**/vite.config.*.timestamp-*',
+
+  '**/CHANGELOG*.md',
+  '**/*.min.*',
+  '**/LICENSE*',
+  '**/__snapshots__',
+  '**/auto-import?(s).d.ts',
+  '**/components.d.ts',
+] as const;
--- src/types.ts
+++ src/types.ts
@@ -5,8 +5,13 @@ export type Awaitable<T> = T | Promise<T>
 
 export type Config = Linter.Config<Linter.RulesRecord>
 
+
 export interface Options {
     packageJson?: boolean
     tsconfig?: boolean
     jsonc?: boolean
+    ignores?: UserIgnores
 }
+
+
+export type UserIgnores = Awaitable<string[]> | (() => Awaitable<string[]>);
