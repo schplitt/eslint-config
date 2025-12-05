cfdd4fce8f7be1a2d35f094f25dc900aac7b15be
schplitt <schplitt.contact@gmail.com>
2025-12-05 20:27:02 +0100

    feat: add  package.json support

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
 import type { Linter } from 'eslint'
 
+export type Awaitable<T> = T | Promise<T>
+
+
 export type Config = Linter.Config<Linter.RulesRecord>
+
+export interface Options {
+    packageJson?: boolean
+    tsconfig?: boolean
+    jsonc?: boolean
+}
